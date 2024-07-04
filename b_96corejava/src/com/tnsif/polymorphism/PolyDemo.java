40b2a57a4e0e6449e3b3e2afe962253c7f84870e
PAVITHRA K <pavithrak1743@gmail.com>
2024-07-04 19:30:39 +0530

    polymorphism commit

---
+++ b_96corejava/src/com/tnsif/polymorphism/PolyDemo.java
@@ -0,0 +1,10 @@
+package com.tnsif.polymorphism;
+
+public class PolyDemo {
+	public static void main(String args[]) {
+		Point pone=new Point();
+		System.out.println(pone);
+		Point ptwo=new Point(2.3f,8.9f);
+		System.out.println(ptwo);
+	}
+}
